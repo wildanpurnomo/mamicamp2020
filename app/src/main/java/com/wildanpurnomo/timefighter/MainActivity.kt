e87fb2d25736d6bede04d304a3fe283970669ba5
Wildan F Purnomo <wildanfajarpurnomo@gmail.com>
2020-05-16 14:43:05 +0700

    Adding Activity Properties

--- app/src/main/java/com/wildanpurnomo/timefighter/MainActivity.kt
+++ app/src/main/java/com/wildanpurnomo/timefighter/MainActivity.kt
@@ -2,11 +2,18 @@ package com.wildanpurnomo.timefighter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var tapMeButton: Button
+    internal lateinit var gameScoreTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tapMeButton = findViewById(R.id.tapMeButton)
+        gameScoreTextView = findViewById(R.id.gameScoreTextView)
     }
 }
