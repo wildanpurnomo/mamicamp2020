2a99ed3f3c138df9120cbad90b3a8a32111221dd
Wildan F Purnomo <wildanfajarpurnomo@gmail.com>
2020-05-16 14:43:58 +0700

    Challenge: Add a Property

--- app/src/main/java/com/wildanpurnomo/timefighter/MainActivity.kt
+++ app/src/main/java/com/wildanpurnomo/timefighter/MainActivity.kt
@@ -9,11 +9,13 @@ class MainActivity : AppCompatActivity() {
 
     internal lateinit var tapMeButton: Button
     internal lateinit var gameScoreTextView: TextView
+    internal lateinit var timeLeftTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tapMeButton = findViewById(R.id.tapMeButton)
         gameScoreTextView = findViewById(R.id.gameScoreTextView)
+        timeLeftTextView = findViewById(R.id.timeLeftTextView)
     }
 }
