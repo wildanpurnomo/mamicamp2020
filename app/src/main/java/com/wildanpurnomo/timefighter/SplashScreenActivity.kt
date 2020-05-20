fd9035559d58116146fd9f75eaefd590f9d85327
Wildan F Purnomo <wildanfajarpurnomo@gmail.com>
2020-05-20 07:45:05 +0700

    Add Splash Screen

---
+++ app/src/main/java/com/wildanpurnomo/timefighter/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.wildanpurnomo.timefighter
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashTime = 3000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, splashTime)
+    }
+}
